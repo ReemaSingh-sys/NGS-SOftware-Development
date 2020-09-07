7fd6512d9090bd7c15f701b117689a1a32c57867
ReemaSingh-sys <70909018+ReemaSingh-sys@users.noreply.github.com>
2020-09-07 14:09:14 -0600

    Update Variantcalling.pl

--- scripts/Variantcalling.pl
+++ scripts/Variantcalling.pl
@@ -6,7 +6,7 @@ my $In = $ARGV[0]; # a test file listed all bam files in it
 my $ref = $ARGV[1]; # N. gonorrhoeae NFA1090.fasta
 
 open (INPUT, $In);
-while( my $bam =<INPUT>)}
+while( my $bam =<INPUT>){
 chomp($bam);
 my $outB = $bam;
 $outB = ~ s/.bam/.bam_sorted/g;
@@ -21,6 +21,6 @@ system "samtools index $outB\n";
 ##### Variant calling and write the output in VCF file
 
 system "samtools mpileup -uf $ref $outB | bcftools view -vcg - >$outV\n";
- 
+} 
 
 
