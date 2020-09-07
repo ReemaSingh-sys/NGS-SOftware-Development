5a01dc7dac77ae053977b56e855af6cab74858ce
ReemaSingh-sys <70909018+ReemaSingh-sys@users.noreply.github.com>
2020-09-07 11:30:11 -0600

    Add files via upload

---
+++ scripts/Variantcalling.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $In = $ARGV[0]; # a test file listed all bam files in it
+my $ref = $ARGV[1]; # N. gonorrhoeae NFA1090.fasta
+
+open (INPUT, $In);
+while( my $bam =<INPUT>)}
+chomp($bam);
+my $outB = $bam;
+$outB = ~ s/.bam/.bam_sorted/g;
+my $outV = $bam;
+$outV = ~ s/.bam/.bam.vcf/g;
+
+##### First sort and index the bam file
+
+system "samtools sort $bam $outB\n";
+system "samtools index $outB\n";
+
+##### Variant calling and write the output in VCF file
+
+system "samtools mpileup -uf $ref $outB | bcftools view -vcg - >$outV\n";
+ 
+
+
